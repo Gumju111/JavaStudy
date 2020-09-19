3fdfc2f348b12d3b0632a4b41a4a17f329c60d2d
forHyelim <formyhye@gmail.com>
2020-09-19 19:33:35 +0900

    4-1,2,3

---
+++ src/chapter4/RectApp.java
@@ -0,0 +1,23 @@
+package chapter4;
+import java.util.Scanner;
+
+class Rectangle{
+	int width;
+	int height;
+	public int getArea() {
+		return width * height;
+	}
+}
+
+public class RectApp {
+	public static void main(String[] args) {
+		Rectangle rect = new Rectangle();	// 객체 생성
+		Scanner scanner = new Scanner(System.in);
+		System.out.print(">> ");
+		rect.width = scanner.nextInt();
+		rect.height = scanner.nextInt();
+		System.out.println("사각형의 면적은 " + rect.getArea());
+		scanner.close();
+	}
+
+}
