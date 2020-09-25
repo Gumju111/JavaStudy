acd2830d37063c45fe9fc8b2709714f80f9ff9f1
forHyelim <formyhye@gmail.com>
2020-09-25 23:56:42 +0900

    5-1

--- src/chapter5/ColorPointEx.java
+++ src/chapter5/ColorPointEx.java
@@ -5,6 +5,9 @@ class Point{
 	public void set(int x, int y) {
 		this.x = x; this.y = y;
 	}
+	public void showPoint() {
+		System.out.println("(" + x + "," + y + ")");
+	}
 }
 public class ColorPointEx {
 	public static void main(String[] args) {
