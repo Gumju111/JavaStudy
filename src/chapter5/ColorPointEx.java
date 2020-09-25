63f04f6a66d58cc7791efd46ac5016f00516ebbb
forHyelim <formyhye@gmail.com>
2020-09-25 23:55:46 +0900

    5-1

--- src/chapter5/ColorPointEx.java
+++ src/chapter5/ColorPointEx.java
@@ -1,5 +1,11 @@
 package chapter5;
 
+class Point{
+	private int x, y;	// 한 점을 구성하는 x, y 좌표
+	public void set(int x, int y) {
+		this.x = x; this.y = y;
+	}
+}
 public class ColorPointEx {
 	public static void main(String[] args) {
 
