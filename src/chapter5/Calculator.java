ce61fe9f384448fef22406c1be2756de555af96f
forHyelim <formyhye@gmail.com>
2020-09-30 23:05:09 +0900

    5-7

---
+++ src/chapter5/Calculator.java
@@ -0,0 +1,8 @@
+package chapter5;
+
+abstract class Calculator {
+	public abstract int add(int a, int b);		// 두 정수의 합을 구하여 리턴
+	public abstract int subtract(int a, int b);	// 두 정수의 차를 구하여 리턴
+	public abstract double average(int[] a);	// 배열에 저장된 정수의 평균 리턴
+
+}
