e8421033c1f0816a521e4e87b672de69182254b1
forHyelim <formyhye@gmail.com>
2020-09-30 22:58:08 +0900

    5-6

---
+++ src/chapter5/Overriding.java
@@ -0,0 +1,19 @@
+package chapter5;
+
+class Cannon extends Weapon{
+	@Override
+	protected int fire() {	// 오버라이딩
+		return 10;	// 대포는 한 번에 10명을 살상
+	}
+}
+public class Overriding {
+	public static void main(String[] args) {
+		Weapon weapon;
+		weapon = new Weapon();
+		System.out.println("기본 무기의 살상 능력은 "+ weapon.fire());
+		
+		weapon = new Cannon();
+		System.out.println("대포의 살상 능력은 "+ weapon.fire());
+	}
+
+}
---
+++ src/chapter5/Weapon.java
@@ -0,0 +1,7 @@
+package chapter5;
+
+public class Weapon {
+	protected int fire() {
+		return 1;	// 무기는 기본적으로 한 명만 살상
+	}
+}
