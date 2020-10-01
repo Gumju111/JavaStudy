67a24aa03604b2d4fe9515faca65ec3a0aaf1cce
forHyelim <formyhye@gmail.com>
2020-10-01 15:59:17 +0900

    5-8

---
+++ src/chapter5/InterfaceEx.java
@@ -0,0 +1,35 @@
+package chapter5;
+
+interface PhoneInterface{		// 인터페이스 선언
+	final int TIMEOUT = 10000;	// 상수 필드 선언
+	void sendCall();			// 추상 메소드
+	void receiveCall();			// 추상 메소드
+	default void printlogo() {	// default 메소드
+		System.out.println("** Phone **");
+	}
+}
+
+class SamsungPhone implements PhoneInterface {	// 인터페이스 구현
+	// PhoneInterface의 모든 추상 메소드 구현
+	@Override
+	public void sendCall() {
+		System.out.println("띠리리리리링");
+	}
+
+	@Override
+	public void receiveCall() {
+		System.out.println("전화가 왔습니다.");
+	}
+	
+	// 메소드 추가 작성
+	public void flash() {System.out.println("전화기에 불이 켜졌습니다.");}
+}
+public class InterfaceEx {
+	public static void main(String[] args) {
+		SamsungPhone phone = new SamsungPhone();
+		phone.printlogo();
+		phone.sendCall();
+		phone.receiveCall();
+		phone.flash();
+	}
+}
