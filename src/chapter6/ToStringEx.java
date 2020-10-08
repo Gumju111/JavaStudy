623856b0a34eded2cef7cc6d8305ef41b1a46ffe
forHyelim <formyhye@gmail.com>
2020-10-08 17:47:22 +0900

    6-2

---
+++ src/chapter6/ToStringEx.java
@@ -0,0 +1,21 @@
+package chapter6;
+
+class Point2{
+	int x, y;
+	public Point2(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public String toString() {
+		return "Point(" + x + "," + y + ")";
+	}
+}
+public class ToStringEx {
+	public static void main(String[] args) {
+		Point2 p = new Point2(2,3);
+		System.out.println(p.toString());
+		System.out.println(p);
+		System.out.println(p + "¿‘¥œ¥Ÿ.");
+	}
+
+}
