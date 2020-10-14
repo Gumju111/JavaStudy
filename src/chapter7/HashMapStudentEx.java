8053d84d16f546dbf35b19772cfed589ecdda998
forHyelim <formyhye@gmail.com>
2020-10-14 21:19:26 +0900

    7-7

---
+++ src/chapter7/HashMapStudentEx.java
@@ -0,0 +1,34 @@
+package chapter7;
+import java.util.*;
+
+class Student { 
+	private int id;
+	private String tel;
+	public Student(int id, String tel) {this.id = id; this.tel = tel;}
+	public int getId() {return id;}
+	public String getTel() {return tel;}
+}
+
+public class HashMapStudentEx {
+	public static void main(String[] args) {
+		// (학생 이름, Student 객체)를 저장하는 해시맵 생성
+		HashMap<String, Student> map = new HashMap<String, Student>();
+		map.put("황황황", new Student(1, "010-1111-1111"));	// 3명의 학생 저장
+		map.put("이이이", new Student(2, "010-2222-2222"));
+		map.put("김김김", new Student(3, "010-3333-3333"));
+		
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			System.out.print("검색할 이름?");
+			String name = scanner.nextLine();	// 사용자로부터 이름 입력
+			if(name.equals("exit"))
+				break;	// while문을 벗어나 프로그램 종료
+			Student student = map.get(name);	// 이름에 해당하는 Student 객체 검색
+			if(student == null)
+				System.out.println(name + "은 없는 사람입니다.");
+			else
+				System.out.println("id:"+student.getId() + "전화:" + student.getTel());
+		}
+		scanner.close();
+	}
+}
