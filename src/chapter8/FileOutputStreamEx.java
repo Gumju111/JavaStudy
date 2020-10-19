d70c627ce9b30cb9ad5326c88859a01358c3d4f6
forHyelim <formyhye@gmail.com>
2020-10-20 06:58:08 +0900

    8-5

---
+++ src/chapter8/FileOutputStreamEx.java
@@ -0,0 +1,19 @@
+package chapter8;
+import java.io.*;
+
+public class FileOutputStreamEx {
+	public static void main(String[] args) {
+		byte b[] = {7,51,3,4,-1,24};
+		
+		try {
+			FileOutputStream fout = new FileOutputStream("c:\\Temp\\test.out");
+			for(int i=0; i<b.length; i++)
+				fout.write(b[i]);	// 배열 b의 바이너리를 그래도 기록
+			fout.close();
+		} catch (IOException e) {
+			System.out.println("c:\\Temp\\test.out에 저장할 수 없습니다. 경로명을 확인해 주세요.");
+			return;
+		}
+		System.out.println("c:\\Temp\\test.out을 저장하였습니다.");
+	}
+}
